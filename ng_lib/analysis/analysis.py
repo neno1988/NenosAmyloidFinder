ce5502081eaab51b28fdeae197590b138ddd2ff2
neno1988 <lorenzogarbani@hotmail.com>
2025-12-08 17:21:15 +0000

    new test for protein analysis

--- ng_lib/analysis/analysis.py
+++ ng_lib/analysis/analysis.py
@@ -1,43 +1,77 @@
+import sys
+from pathlib import Path
+
+# add project root to path
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 import plotly as plt
-import ng_lib as ng
+from ng_lib.data_gathering import get_SEG_data, get_zipperDB_data, get_amylpred_data
+from ng_lib.utils import FastaSeq
+from ng_lib.plot import heatmaps_binary_non_binary
+from ng_lib.visualization.plotly_heatmaps import create_interactive_heatmaps
 import os
 import json
 import re
 import numpy as np
 
-def analyse_protein(output_folder, seq, description, name, threshold, SEG, xticks):
-    fasta_protein = ng.utils.FastaSeq() # TODO: use official fasta struct here
+
+# Define the CDC19 protein sequence in FASTA format
+CDC19_FASTA_FULL = """>sp|P00549|KPYK1_YEAST Pyruvate kinase 1 OS=Saccharomyces cerevisiae (strain ATCC 204508 / S288c) OX=559292 GN=CDC19 PE=1 SV=2
+MSRLERLTSLNVVAGSDLRRTSIIGTIGPKTNNPETLVALRKAGLNIVRMNFSHGSYEYH
+KSVIDNARKSEELYPGRPLAIALDTKGPEIRTGTTTNDVDYPIPPNHEMIFTTDDKYAKA
+CDDKIMYVDYKNITKVISAGRIIYVDDGVLSFQVLEVVDDKTLKVKALNAGKICSHKGVN
+LPGTDVDLPALSEKDKEDLRFGVKNGVHMVFASFIRTANDVLTIREVLGEQGKDVKIIVK
+IENQQGVNNFDEILKVTDGVMVARGDLGIEIPAPEVLAVQKKLIAKSNLAGKPVICATQM
+LESMTYNPRPTRAEVSDVGNAILDGADCVMLSGETAKGNYPINAVTTMAETAVIAEQAIA
+YLPNYDDMRNCTPKPTSTTETVAASAVAAVFEQKAKAIIVLSTSGTTPRLVSKYRPNCPI
+ILVTRCPRAARFSHLYRGVFPFVFEKEPVSDWTDDVEARINFGIEKAKEFGILKKGDTYV
+SIQGFKAGAGHSNTLQVSTV"""
+
+
+def analyse_protein(output_folder, seq, description, name, threshold, SEG, xticks, 
+                    debug_SEG_data = None, debug_zipperDB_data = None, debug_amylpred_data = None):
+    #TODO: this function might belong in the upper sub-package
+    fasta_protein = FastaSeq() # TODO: use official fasta struct here
     fasta_protein.description = description
     fasta_protein.seq = seq
     fasta_protein.name = name
 
     # Get SEG and ZipperDB data
-    seg_data = ng.get_SEG_data(fasta_protein, seg=SEG)
-    zipperDB_data = ng.get_zipperDB_data(fasta_protein)
+    if debug_SEG_data is not None:
+        seg_data = debug_SEG_data
+    else:
+        seg_data = get_SEG_data(fasta_protein, seg=SEG)
+
+    if debug_zipperDB_data is not None:
+        zipperDB_data = debug_zipperDB_data
+    else:
+        zipperDB_data = get_zipperDB_data(fasta_protein)
 
     # Get Amylpred data if credentials are provided
-    try:
-        amylpred_data = ng.get_amylpred_data(fasta_protein.seq)
-    except Exception as e:
-        # TODO: reinstate warning
-        # messagebox.showwarning("Warning", f"Failed to get Amylpred data: {str(e)}")
-        print("Reinstate warning about Amylpred failure here")
-        amylpred_data = None
+    if debug_amylpred_data is not None:
+        amylpred_data = debug_amylpred_data
+    else:
+        try:
+            amylpred_data = get_amylpred_data(fasta_protein.seq)
+        except Exception as e:
+            # TODO: reinstate warning
+            # messagebox.showwarning("Warning", f"Failed to get Amylpred data: {str(e)}")
+            print("Reinstate warning about Amylpred failure here")
+            amylpred_data = None
 
     do_matplotlib_plots = False
     if do_matplotlib_plots:
         if SEG==0:
             # TODO: only return fig  in nice_heatmap_plot?
             white_cmap = ["white", "white"]
-            fig = ng.heatmaps_binary_non_binary(seg_data, zipperDB_data[0], threshold=threshold, name=name, force_cmap=white_cmap, seg_bar_height=0, xticks=xticks)
+            fig = heatmaps_binary_non_binary(seg_data, zipperDB_data[0], threshold=threshold, name=name, force_cmap=white_cmap, seg_bar_height=0, xticks=xticks)
             # Save the figure as a JPEG file
             file_name = fasta_protein.name+'_treshold_'+str(threshold)+'.jpg'
             fig.savefig(os.path.join(output_folder, file_name), format='jpeg', dpi=300)
             fig.show() 
 
         else:
-            fig = ng.heatmaps_binary_non_binary(seg_data, zipperDB_data[0], threshold=threshold, name=name, xticks=xticks)
+            fig = heatmaps_binary_non_binary(seg_data, zipperDB_data[0], threshold=threshold, name=name, xticks=xticks)
             # Save the figure as a JPEG file
             file_name = fasta_protein.name+'_treshold_'+str(threshold)+'.jpg'
             fig.savefig(os.path.join(output_folder, file_name), format='jpeg', dpi=300)
@@ -46,7 +80,7 @@ def analyse_protein(output_folder, seq, description, name, threshold, SEG, xtick
     # Create interactive plot
     IMAGE_HEIGHT = 200
     IMAGE_WIDTH = 1800
-    fig = ng.visualization.plotly_heatmaps.create_interactive_heatmaps(
+    fig = create_interactive_heatmaps(
         lcr_data=seg_data.reshape(1, -1),
         seg_treshold = SEG,
         zipperdb_data=zipperDB_data,
@@ -104,3 +138,60 @@ def analyse_protein(output_folder, seq, description, name, threshold, SEG, xtick
         plt.title(f'Amylpred Analysis - {name}')
         plt.show()
 
+
+def test_analysis():
+    with open(os.getcwd() + f'\\test_files\\test_data_CDC19.json', 'r') as f:
+        test_data = json.load(f)
+    threshold = -23
+    SEG = 25
+    xticks = 0
+    # TODO: separation of concerns could be better here
+    sequence = test_data["fasta_seq"]
+    output_folder = os.getcwd() + f'\\test_files\\'
+    analyse_protein(output_folder, sequence, test_data["fasta_description"], test_data["fasta_name"], threshold, SEG, xticks, 
+                    debug_SEG_data = np.array(test_data["SEG_data"]), 
+                    debug_zipperDB_data = np.array(test_data["zipperDB_data"]), 
+                    debug_amylpred_data = np.array(test_data["amylpred_data"]))
+
+
+# TEST DATA GENERATION FUNCTIONS
+def generate_test_data():
+    fasta_cdc19 = get_CDC19_fasta()
+    SEG_data = get_SEG_data(fasta_cdc19, seg=25)
+    zipperDB_data = get_zipperDB_data(fasta_cdc19)
+    amylpred_data = get_amylpred_data(fasta_cdc19.seq)
+
+    test_data = {
+        "fasta_seq": fasta_cdc19.seq,
+        "fasta_description": fasta_cdc19.description,
+        "fasta_name": fasta_cdc19.name,
+        "SEG_data": SEG_data.tolist(),
+        "zipperDB_data": zipperDB_data.tolist(),
+        "amylpred_data": amylpred_data.tolist()
+    }
+
+    with open(os.getcwd() + f'\\test_files\\test_data_CDC19.json', 'w') as f:
+        json.dump(test_data, f, indent=4)
+       
+
+def get_CDC19_fasta():
+    fasta = FastaSeq()
+    fasta.description = """>sp|P00549|KPYK1_YEAST Pyruvate kinase 1 OS=Saccharomyces cerevisiae (strain ATCC 204508 / S288c) OX=559292 GN=CDC19 PE=1 SV=2"""
+    fasta.seq = """MSRLERLTSLNVVAGSDLRRTSIIGTIGPKTNNPETLVALRKAGLNIVRMNFSHGSYEYH\
+KSVIDNARKSEELYPGRPLAIALDTKGPEIRTGTTTNDVDYPIPPNHEMIFTTDDKYAKA\
+CDDKIMYVDYKNITKVISAGRIIYVDDGVLSFQVLEVVDDKTLKVKALNAGKICSHKGVN\
+LPGTDVDLPALSEKDKEDLRFGVKNGVHMVFASFIRTANDVLTIREVLGEQGKDVKIIVK\
+IENQQGVNNFDEILKVTDGVMVARGDLGIEIPAPEVLAVQKKLIAKSNLAGKPVICATQM\
+LESMTYNPRPTRAEVSDVGNAILDGADCVMLSGETAKGNYPINAVTTMAETAVIAEQAIA\
+YLPNYDDMRNCTPKPTSTTETVAASAVAAVFEQKAKAIIVLSTSGTTPRLVSKYRPNCPI\
+ILVTRCPRAARFSHLYRGVFPFVFEKEPVSDWTDDVEARINFGIEKAKEFGILKKGDTYV\
+SIQGFKAGAGHSNTLQVSTV"""
+    fasta.name = "CDC19"
+    return fasta
+
+
+
+if __name__ == "__main__":
+    # generate_test_data() # generates test data in test_files based on CDC19 fasta.
+    test_analysis()
+    print("DONE")
